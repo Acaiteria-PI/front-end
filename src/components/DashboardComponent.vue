00071e665eb393855f07797b84da0f990012bb9b
ManoelFranciscoDias <manoel2007dias@gmail.com>
2025-05-25 21:20:48 -0300

    feat: create dashboard #1

---
+++ src/components/DashboardComponent.vue
@@ -0,0 +1,79 @@
+<script setup></script>
+
+<template>
+<div class="container">
+<div class="title">
+  <h1>Dashboard</h1>
+</div>
+<div class="boxes">
+  <div class="box-pedidos-hoje">
+    <p class="box-title">Pedidos de Hoje</p>
+    <p class="box-number">24</p>
+  </div>
+
+  <div class="box-estoque-baixo">
+    <p class="box-title">Estoque Baixo</p>
+    <p class="box-number">3</p>
+  </div>
+
+  <div class="box-entregas">
+    <p class="box-title">Entregas Realizadas</p>
+    <p class="box-number">12</p>
+  </div>
+</div>
+
+</div>
+</template>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 60px;
+  margin-left: 50px;
+}
+
+.boxes {
+  display: flex;
+  gap: 40px;
+  margin-top: 20px;
+}
+
+.box-pedidos-hoje {
+  background-color: #F6C98E;
+  width: 160px;
+  height: 90px;
+  padding: 10px;
+  border-radius: 16px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  font-weight: bold;
+}
+
+.box-estoque-baixo {
+  background-color: #8CC3A777;
+  width: 160px;
+  height: 90px;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 16px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+
+.box-entregas {
+  background-color: #EEA5A0;
+  width: 160px;
+  height: 90px;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 16px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+
+.box-number {
+  text-align: center;
+  margin-top: 10px;
+  font-size: 20pt;
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,100 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <div class="nav-left">
+        <img class="logo" src="/src/assets/img/Logo.png" alt="" />
+      </div>
+
+      <ul class="nav-menu">
+        <li class="nav-item">Início</li>
+        <li class="nav-item">Menu de gerenciamento</li>
+        <li class="nav-item">Cardápio</li>
+      </ul>
+
+      <div class="nav-right">
+        <i class="fas fa-shopping-bag"></i>
+        <button class="btn-login">LOGIN</button>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fffbf4;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  height: 80px;
+  padding: 0 50px;
+}
+
+.nav-left,
+.nav-right {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.logo {
+  width: 64;
+  height: 55px;
+  cursor: pointer;
+}
+
+.nav-menu {
+  display: flex;
+  gap: 30px;
+  list-style: none;
+  justify-content: center;
+}
+
+.nav-item {
+  height: 19px;
+  cursor: pointer;
+  position: relative;
+  transition: color 0.5s ease;
+}
+
+.nav-item::after {
+  content: '';
+  position: absolute;
+  left: 50%;
+  bottom: -8px;
+  transform: translateX(-50%) scaleY(0);
+  transform-origin: bottom;
+  height: 1.5px;
+  width: 100%;
+  background-color: black;
+  opacity: 0;
+  transition: transform 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55), opacity 0.3s ease;
+}
+
+.nav-item:hover::after {
+  transform: translateX(-50%) scaleY(1);
+  opacity: 1;
+}
+
+.btn-login {
+  background-color: #682c48;
+  color: #fff;
+  border: none;
+  width: 103px;
+  height: 40px;
+  border-radius: 30px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.btn-login:hover {
+  background-color: #a3024d;
+}
+
+.fas.fa-shopping-bag {
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
