f0d7f9c3263167c7daddc6b514da35c761aa5c9c
joaovnpt <joaovitoguiotti8@gmail.com>
2025-06-13 15:37:26 -0300

    REFACTOR: Componentize dashboard and refactor code for better organization #1

---
+++ src/components/StockGraph.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="content-estoque">
+    <p class="section-title">Estoque Baixo</p>
+    <div class="estoque"></div>
+  </div>
+</template>
+
+<style scoped>
+.content-estoque {
+  display: flex;
+  flex-direction: column;
+}
+.section-title {
+  font-weight: bold;
+  font-size: 1.rem;
+  margin-bottom: 12px;
+}
+.estoque {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  width: 366px;
+  height: 314px;
+  border-radius: 16px;
+}
+</style>
