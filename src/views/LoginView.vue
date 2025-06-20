e54b18ba0a2e9b24c8de19a65f8246b7c1efab22
joaovnpt <joaovitoguiotti8@gmail.com>
2025-06-19 21:59:56 -0300

    CHORE: Install tailwindcss

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -16,50 +16,8 @@ const handleLogin = async () => {
 }
 </script>
 <template>
-  <div class="container">
-    <div class="img">
-      <img src="/src/assets/img/Logo.png" alt="" />
-    </div>
-    <h1>Logar-se</h1>
-    <form @submit.prevent="handleLogin">
-      <input v-model="email" type="email" placeholder="Email" />
-      <input v-model="password" type="password" placeholder="Senha" />
-      <button type="submit">Logar</button>
-    </form>
-  </div>
+  
 </template>
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-  height: 90vh;
-}
 
-.img {
-  width: 4rem;
-  height: 3.5rem;
-}
-
-input {
-  width: 20rem;
-  height: 3.75rem;
-  border-radius: 0.75rem;
-  border: 1px solid black;
-}
-
-input::placeholder {
-  padding: 0 1rem;
-}
-
-button {
-  width: 20rem;
-  height: 3.75rem;
-  border-radius: 0.75rem;
-  background-color: #682c48;
-  border: none;
-  color: white;
-}
 </style>
