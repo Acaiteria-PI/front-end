7783bf34538f52e4f8b6f5e857c220b53356edd7
joaovnpt <joaovitoguiotti8@gmail.com>
2025-07-17 11:41:32 -0300

    FIX: Adjust CoposProntos list spacing and position

--- src/components/CoposProntos.vue
+++ src/components/CoposProntos.vue
@@ -39,11 +39,11 @@ const combos = [
 <template>
   <div class="mt-16 flex flex-col">
     <h1 class="text-3xl">Copos prontos</h1>
-    <div class="flex flex-wrap justify-between mt-6">
+    <div class="flex flex-wrap justify-start gap-6 mt-6">
       <div class="bg-neutral-300 w-50 h-60 rounded-2xl" v-for="cup in finalCups" :key="cup.id"></div>
     </div>
     <h1 class="text-3xl mt-16">Combos</h1>
-    <div class="flex flex-wrap justify-between mt-6">
+    <div class="flex flex-wrap justify-start gap-6 mt-6">
       <div class="bg-neutral-300 w-50 h-60 rounded-2xl" v-for="combo in combos" :key="combo.id"></div>
     </div>
   </div>
