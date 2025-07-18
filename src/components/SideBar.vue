bbd01fdbce753c599ad9946ffdfed1bc4843f5d7
joaovnpt <joaovitoguiotti8@gmail.com>
2025-07-18 13:57:18 -0300

    FEAT: Refactor SideBar component to use dynamic tab rendering #12

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,57 +2,56 @@
 import { ref } from 'vue'
 import { LogOut, Package, Tag, GlassWater, Gift, Users } from 'lucide-vue-next'
 
-const tabs = ref(['stock', 'products', 'recipients', 'combos', 'employees'])
-const currentTab = ref('stock')
+const tabs = [
+  { name: 'Estoque', icon: Package, route: '/stock' },
+  { name: 'Produtos', icon: Tag, route: '/products' },
+  { name: 'Recipientes', icon: GlassWater, route: '/recipients' },
+  { name: 'Combos', icon: Gift, route: '/combos' },
+]
+const currentTab = ref('/stock')
+
+const enterTab = (tab) => {
+  currentTab.value = tab
+}
 </script>
 
 <template>
-  <nav class="sidebar w-60 h-[calc(100vh-4.5rem)] flex flex-col gap-6 bg-neutral-100 pt-10 pl-6">
-    <p class="text-sm text-neutral-500">Produtos</p>
-    <ul class="flex flex-col gap-8 mb-6">
+  <nav class="sidebar w-60 h-[calc(100vh-4.5rem)] flex flex-col gap-6 bg-neutral-100 pt-10">
+    <p class="text-sm text-neutral-500 ml-6">Produtos</p>
+    <ul class="flex flex-col gap-4 mb- ml-4">
       <li
-        class="flex flex-row items-center gap-2"
-        :class="currentTab == 'stock' ? 'text-gray-950 font-semibold' : 'text-neutral-500'"
+        v-for="tab in tabs"
+        :key="tab.route"
+        @click="enterTab(tab.route)"
+        class="flex flex-row items-center w-[calc(15rem-24px)] h-10 py-4 px-2 gap-2 rounded-lg hover:bg-neutral-300 transition-all cursor-pointer"
+        :class="currentTab == tab.route ? 'text-gray-950 font-semibold' : 'text-neutral-500'"
       >
-        <Package :size="20" />
-        <router-link>Estoque</router-link>
-      </li>
-      <li
-        class="flex flex-row gap-2 items-center"
-        :class="currentTab == 'products' ? 'text-gray-950 font-semibold' : 'text-neutral-500'"
-      >
-        <Tag :size="20" class="text-neutral-500" />
-        <router-link>Produtos</router-link>
-      </li>
-      <li
-        class="flex flex-row gap-2 items-center"
-        :class="currentTab == 'recipients' ? 'text-gray-950 font-semibold' : 'text-neutral-500'"
-      >
-        <GlassWater :size="20" class="text-neutral-500" />
-        <router-link>Recipientes</router-link>
-      </li>
-      <li
-        class="flex flex-row gap-2 items-center"
-        :class="currentTab == 'combos' ? 'text-gray-950 font-semibold' : 'text-neutral-500'"
-      >
-        <Gift :size="20" class="text-neutral-500" />
-        <router-link>Combos</router-link>
+        <div
+          v-if="tab.route == currentTab"
+          class="identifier w-1.5 h-8 bg-pink-900 rounded-lg"
+        ></div>
+        <component :is="tab.icon" :size="20" />
+        <router-link>{{ tab.name }}</router-link>
       </li>
     </ul>
 
-    <p class="text-sm text-neutral-500">Equipe</p>
+    <p class="text-sm text-neutral-500 ml-6">Equipe</p>
     <div
-      class="flex flex-row gap-2 items-center"
+      @click="enterTab('employees')"
+      class="flex flex-row items-center w-[calc(15rem-24px)] h-10 ml-4 py-4 px-2 gap-2 rounded-lg hover:bg-neutral-300 transition-all cursor-pointer"
       :class="currentTab == 'employees' ? 'text-gray-950 font-semibold' : 'text-neutral-500'"
     >
+      <div v-if="currentTab == 'employees'" class="identifier w-1.5 h-8 bg-pink-900 rounded-lg"></div>
       <Users :size="20" />
       <router-link>Funcionários</router-link>
     </div>
 
-    <div class="logout mt-auto">
-      <router-link to="/login" class="flex flex-row items-center">
+    <div
+      class="logout mt-auto mb-6 w-fit py-2 px-2 ml-4 rounded-xl hover:bg-neutral-300 transition-colors"
+    >
+      <router-link to="/login" class="flex flex-row gap-2 items-center text-red-400">
         <LogOut :size="20" color="#ff6467" />
-        <button class="text-red-400 p-2 rounded">Sair</button>
+        Sair
       </router-link>
     </div>
   </nav>
