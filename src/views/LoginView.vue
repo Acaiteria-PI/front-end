f49ca4322a75ced7b998c24ba1d5af1bc7a12fa7
joaovnpt <joaovitorguiotti8@gmail.com>
2025-08-16 16:22:42 -0300

    FEAT: Add error message when sign in fail #12

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { useAuth } from '@/composables/auth'
 import { useLoading } from '@/stores/loading.js'
+import { OctagonX } from 'lucide-vue-next'
 
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/css/index.css';
@@ -11,13 +12,13 @@ const loadingStore = useLoading()
 const email = ref('')
 const password = ref('')
 const { login } = useAuth()
+const errorMessage = ref('')
 
 const handleLogin = async () => {
   try {
     await login(email.value, password.value)
-    alert('Login successful!')
   } catch (err) {
-    alert('Login failed: ')
+    errorMessage.value = err.response?.data.detail
   }
 }
 </script>
@@ -30,22 +31,26 @@ const handleLogin = async () => {
     </div>
     <div class="login-container flex flex-col gap-8">
       <h1 class="text-3xl font-medium mx-auto">Seja bem-vindo!</h1>
-      <form @submit.prevent="handleLogin" class="flex flex-col gap-8">
+      <form @submit.prevent="handleLogin" class="flex flex-col">
         <input
           type="email"
           placeholder="Email"
           v-model="email"
-          class="w-full h-15 flex items-center p-4 border border-solid border-black rounded-xl"
+          class="w-full h-15 flex items-center p-4 mb-8 border border-solid border-black rounded-xl"
         />
         <input
           type="password"
           placeholder="Password"
           v-model="password"
-          class="w-full h-15 flex items-center p-4 border border-solid border-black rounded-xl"
+          class="w-full h-15 flex items-center p-4 mb-4 border border-solid border-black rounded-xl"
         />
+        <div v-if="errorMessage && password" class="error-message flex gap-2 mt--4 justify-center align-center">
+          <OctagonX :size="16" class="text-red-800" />
+          <p class="text-red-800 text-sm">{{ errorMessage }}</p>
+        </div>
         <button
           type="submit"
-          class="w-full h-15 rounded-xl bg-pink-950 text-white font-bold cursor-pointer hover:bg-rose-950 transition delay-150ms ease-in-out"
+          class="w-full h-15 mt-4 rounded-xl bg-pink-950 text-white font-bold cursor-pointer hover:bg-rose-950 transition delay-150ms ease-in-out"
         >
           Logar
         </button>
