9869ed05480f270a7577dfcec3d7aa9e05afad6e
joaovnpt <joaovitorguiotti8@gmail.com>
2025-08-22 08:21:43 -0300

    FEAT: Add flex layout to products section #13

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,26 +1,26 @@
 <script setup>
 import SectionTitle from '@/components/management-menu/SectionTitle.vue'
 import SearchBar from '@/components/management-menu/SearchBar.vue'
-import NewProductBtn from '@/components/management-menu/NewProductBtn.vue';
-import CardsProduct from '@/components/management-menu/products-section/CardsProduct.vue';
+import NewProductBtn from '@/components/management-menu/NewProductBtn.vue'
+import CardsProduct from '@/components/management-menu/products-section/CardsProduct.vue'
 </script>
 
 <template>
   <div class="w-full p-8">
     <SectionTitle title="Produtos" class="mt-8" />
-
-    <section class="flex flex-row itens-start justify-between">
-      <SearchBar /> <NewProductBtn />
+    <section class="flex flex-row items-start justify-between">
+      <SearchBar />
+      <NewProductBtn />
     </section>
+    <section class="w-full h-100 flex flex-wrap gap-6">
+      <CardsProduct />
+      <CardsProduct />
+      <CardsProduct />
+      <CardsProduct />
+      <CardsProduct />
+      <CardsProduct />
 
-    <div class="flex flex-row gap-[24px] mt-4">
-      <CardsProduct />
-      <CardsProduct />
-    </div>
-    <div class="flex flex-row gap-[24px]">
-      <CardsProduct />
-      <CardsProduct />
-    </div>
+    </section>
   </div>
 </template>
 
