094c1e72c3e46c8b8ebea0d99a81dabab126b7e5
joaovnpt <joaovitorguiotti8@gmail.com>
2025-09-05 11:51:38 -0300

    FEAT: Create modal for ingredient creation #12

--- src/stores/ingredient.js
+++ src/stores/ingredient.js
@@ -5,14 +5,20 @@ import IngredientApi from '@/services/ingredientApi'
 const ingredientApi = new IngredientApi()
 
 export const useIngredientStore = defineStore('ingredient', () => {
+
   const ingredients = ref([])
+  const isModalOpen = ref(false)
 
   const fetchIngredients = async () => {
     const data = await ingredientApi.fetchIngredients()
     ingredients.value = data.results ?? data
   }
 
+  const openModal = (() => {
+    isModalOpen.value = true
+  })
+
   return {
-    ingredients, fetchIngredients
+    ingredients, fetchIngredients, isModalOpen, openModal
   }
 })
