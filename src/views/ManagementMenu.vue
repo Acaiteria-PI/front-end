b0a06d49b9757bda9ebdafb61a49cece5e9ccb63
joaovnpt <joaovitorguiotti8@gmail.com>
2025-09-15 13:28:01 -0300

    FEAT: Add title to Management Menu (commented yet) #12

--- src/views/ManagementMenu.vue
+++ src/views/ManagementMenu.vue
@@ -3,13 +3,14 @@ import SideBar from '@/components/sidebar/SideBar.vue'
 </script>
 
 <template>
-  <div class="w-screen flex flex-row">
+  <div class="w-screen h-full flex flex-row">
     <aside>
       <SideBar />
     </aside>
 
-    <main class="w-full">
+    <main class="w-full h-full relative">
       <router-view />
+      <!--      <h1 class="text-4xl font-bold absolute inset-0 m-auto mt-8 text-center">Menu de gerenciamento</h1>-->
     </main>
   </div>
 </template>
