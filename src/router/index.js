ab1d597b2f0ddfc87a16d967c5a8c32efdcc0ba7
joaovnpt <joaovitorguiotti8@gmail.com>
2025-09-19 10:29:53 -0300

    FEAT: Enhance router protection logics with correct fetch timing #12

--- src/router/index.js
+++ src/router/index.js
@@ -61,5 +61,18 @@ router.beforeEach(async (to, from, next) => {
   if (to.name === 'management-menu') next({ name: 'ingredients' })
   else next()
 })
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuth()
+
+  if (!authStore.user && authStore.accessToken) {
+    try {
+      await authStore.fetchCurrentUser()
+    } catch (error) {
+      console.error('Erro ao buscar usuário:', error)
+    }
+  }
+  if (to.name !== 'login' && !authStore.isLoggedIn) next({ name: 'login' })
+  else next()
+})
 
 export default router
