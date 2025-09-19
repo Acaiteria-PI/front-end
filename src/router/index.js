ae3d2b01e09108581270fad3bb329f7c26d2f94c
joaovnpt <joaovitorguiotti8@gmail.com>
2025-09-19 10:33:58 -0300

    FEAT: Import auth store in index.js #12

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuth } from '@/stores/auth.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
