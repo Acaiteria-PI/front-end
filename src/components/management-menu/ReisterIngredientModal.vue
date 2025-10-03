78afd978fc0bd2b028488af45d6c995cba4c5ca2
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-02 23:58:01 -0300

    FEAT: Add Ingredient Create and Update operations, including methods and modal #27

---
+++ src/components/management-menu/ReisterIngredientModal.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { reactive, onMounted } from 'vue'
+import { X } from 'lucide-vue-next'
+import { useIngredientStore } from '@/stores/ingredient.js'
+import { useModalStore } from '@/stores/modal.js'
+
+const ingredientStore = useIngredientStore()
+const modalStore = useModalStore()
+
+defineProps({
+  title: { type: String, required: true },
+  btnName: { type: String, required: true },
+  mode: { type: String, required: true },
+  model: { type: String, required: true }
+})
+
+defineEmits(['createIngredient', 'editIngredient'])
+
+const fields = reactive([
+  {
+    id: 'name',
+    name: 'Nome',
+    placeholder: 'Ex: Banana',
+    type: 'text',
+    cols: '1'
+  },
+  {
+    id: 'portion',
+    name: 'Porção',
+    placeholder: 'Porção para o produto final',
+    type: 'text',
+    cols: '1'
+  },
+  {
+    id: 'price',
+    name: 'Preço',
+    placeholder: 'R$5,00',
+    type: 'text',
+    cols: '1'
+  },
+  {
+    id: 'unit_of_measure',
+    name: 'Unidade de medida',
+    placeholder: 'Un. da porção',
+    type: 'text',
+    cols: '1'
+  }
+])
+
+onMounted(() => {
+  ingredientStore.fetchIngredients()
+})
+</script>
+
+<template>
+  <main
+    class="w-100 h-fit bg-white rounded-3xl shadow-xl p-6 pt-8 flex flex-col items-center gap-6">
+    <h1 class="text-3xl font-bold mb-2 text-center">{{ title }}</h1>
+    <div
+      class="h-8 w-8 rounded-full hover:bg-neutral-200 flex items-center justify-center absolute top-4 right-4 cursor-pointer"
+      @click="modalStore.closeCreateModal">
+      <X />
+    </div>
+
+    <form @submit.prevent="$emit(mode === 'create' ? 'createIngredient' : 'editIngredient')"
+          class="w-full flex flex-col gap-6">
+      <section class="w-full grid grid-cols-1 gap-4">
+        <div v-for="field in fields" :key="field.id"
+             class="flex flex-col gap-1 align-center w-full"
+             :class="{ 'col-span-2' : field.cols === '2', 'col-span-1': field.cols === '1' }">
+          <label :for="field.id">{{ field.name }}</label>
+          <input
+            :id="field.id"
+            :type="field.type"
+            :placeholder="field.placeholder"
+            v-model="model[field.id]"
+            class="border border-neutral-300 rounded-xl p-2 w-full h-12"
+          />
+        </div>
+      </section>
+      <button type="submit"
+              class="w-full h-15 rounded-xl bg-rose-900 font-medium text-white hover:bg-rose-950 cursor-pointer duration-200 ease-in-out">
+        {{ btnName }}
+      </button>
+    </form>
+  </main>
+</template>
+
+<style scoped></style>
--- src/stores/ingredient.js
+++ src/stores/ingredient.js
@@ -1,19 +1,68 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import IngredientApi from '@/services/ingredientApi'
+import { useLoading } from '@/stores/loading.js'
+import { useModalStore } from '@/stores/modal.js'
 
+const loadingStore = useLoading()
+const modalStore = useModalStore()
 const ingredientApi = new IngredientApi()
 
 export const useIngredientStore = defineStore('ingredient', () => {
 
   const ingredients = ref([])
+  const newIngredient = ref({
+    id: null,
+    name: '',
+    portion: '',
+    price: null,
+    unit_of_measure: ''
+  })
 
   const fetchIngredients = async () => {
+    loadingStore.isLoading = true
     const data = await ingredientApi.fetchIngredients()
-    ingredients.value = data.results ?? data
+    ingredients.value = Array.isArray(data.results) ? [...data.results] : [...data]
+    loadingStore.isLoading = false
+  }
+
+  const createIngredient = async (ingredient) => {
+    try {
+      loadingStore.isLoading = true
+      const created = await ingredientApi.createIngredient(ingredient)
+      ingredients.value.push(created)
+
+      newIngredient.value = {
+        id: null,
+        name: '',
+        price: null,
+        unit_of_measure: ''
+      }
+
+      modalStore.closeCreateModal()
+      loadingStore.isLoading = false
+    } catch (err) {
+      console.error('Error creating ingredient', err)
+      loadingStore.isLoading = false
+    }
+  }
+
+  const deleteIngredient = async (id) => {
+    try {
+      loadingStore.isLoading = true
+
+      await ingredientApi.deleteIngredient(id)
+      ingredients.value = ingredients.value.filter(ingredient => ingredient.id !== id)
+
+      modalStore.closeConfirmDeleteModal()
+      loadingStore.isLoading = false
+    } catch (err) {
+      console.error('Error deleting ingredient:', err)
+      loadingStore.isLoading = false
+    }
   }
 
   return {
-    ingredients, fetchIngredients
+    ingredients, newIngredient, fetchIngredients, createIngredient, deleteIngredient
   }
 })
