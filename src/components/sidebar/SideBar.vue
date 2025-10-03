081d15d1497b32f5c9bf42f089b6d78d0f2c3221
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-02 21:42:30 -0300

    FEAT: Change whole navbar for better ui #27

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -13,7 +13,7 @@ const tabs = [
   { name: 'Estoque', icon: Package, route: '/management-menu/stock' },
   { name: 'Produtos', icon: Tag, route: '/management-menu/products' },
   { name: 'Recipientes', icon: GlassWater, route: '/management-menu/recipients' },
-  { name: 'Combos', icon: Gift, route: '/management-menu/combos' },
+  { name: 'Combos', icon: Gift, route: '/management-menu/combos' }
 ]
 const currentTab = computed(() => {
   return route.path
@@ -26,7 +26,9 @@ const enterTab = (tab) => {
 </script>
 
 <template>
-  <nav class="sidebar w-60 h-screen flex flex-col gap-6 bg-neutral-100 pt-10 sticky top-0">
+  <nav
+    class="sidebar w-60 h-[calc(100vh-5.5rem)] flex flex-col rounded-2xl gap-6 bg-neutral-100 pt-10">
+    <!--    5.5rem é o resultado do tamanho da navbar + seu padding (h-18 + pt-4)-->
     <p class="text-sm text-neutral-500 ml-6">Produtos</p>
     <ul class="flex flex-col gap-4 mb- ml-4">
       <SideBarItem
