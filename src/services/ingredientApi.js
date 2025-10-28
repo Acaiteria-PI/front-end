71f283e904d4c5b22d1fb420961fab9ef986e577
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-28 11:29:40 -0300

    FEAT: Format ingredientApi #40

--- src/services/ingredientApi.js
+++ src/services/ingredientApi.js
@@ -1,19 +1,22 @@
 import API from '@/services/axios'
 
 export default class IngredientApi {
-    async fetchIngredients() {
-        const { data } = await API.get('api/ingredients/')
-        return data
-    }
-    async createIngredient(ingredient) {
-        const { data } = await API.post('api/ingredients/', ingredient)
-        return data
-    }
-    async updateIngredient(ingredient) {
-        const { data } = await API.put(`api/ingredients/${ingredient.id}/`, ingredient)
-        return data
-    }
-    async deleteIngredient(id) {
-        await API.delete(`api/ingredients/${id}/`)
-    }
+  async fetchIngredients() {
+    const { data } = await API.get('api/ingredients/')
+    return data
+  }
+
+  async createIngredient(ingredient) {
+    const { data } = await API.post('api/ingredients/', ingredient)
+    return data
+  }
+
+  async updateIngredient(ingredient) {
+    const { data } = await API.put(`api/ingredients/${ingredient.id}/`, ingredient)
+    return data
+  }
+
+  async deleteIngredient(id) {
+    await API.delete(`api/ingredients/${id}/`)
+  }
 }
