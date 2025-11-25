aec6c8b0237887aea0490913b216121fc40be7a4
guiottijoao <joaovitorguiotti8@gmail.com>
2025-11-24 23:29:07 -0300

    FEAT: Create order main page and order creation first step page (CreateOrder)

--- src/components/management-menu/ProductsTable.vue
+++ src/components/management-menu/ProductsTable.vue
@@ -1,9 +1,23 @@
 <script setup>
-import { PencilLine, Trash } from 'lucide-vue-next'
-import { useStockStore } from '@/stores/stock.js'
-import { useModalStore } from '@/stores/modal.js'
+import {PencilLine, Trash} from 'lucide-vue-next'
+import {useStockStore} from '@/stores/stock.js'
+import {useModalStore} from '@/stores/modal.js'
 
-defineProps(['headers', 'products'])
+const props = defineProps({
+  headers: {
+    type: Array,
+    required: true
+  },
+  products: {
+    type: Array,
+    required: true
+  },
+  canDelete: {
+    type: Boolean,
+    required: false,
+    default: true
+  }
+})
 
 const stockStore = useStockStore()
 const modalStore = useModalStore()
@@ -11,11 +25,19 @@ const modalStore = useModalStore()
 const formatField = (product, value) => {
   if (value === 'ingredient_data') return product.ingredient_data?.name || 'N/A'
   else if (value === 'expiration_date') return new Date(product.expiration_date).toLocaleDateString('pt-BR')
+  else if (value === 'order_date') return new Date(product.order_date).toLocaleString("pt-BR", {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
   else if (value === 'price') return `R$ ${product.price.replace('.', ',')}`
   else if (value === 'batch_price') return `R$ ${product.batch_price.replace('.', ',')}`
-  else if (value === 'quantity') return product.quantity.replace('.',',')
+  else if (value === 'quantity') return product.quantity.replace('.', ',')
   else if (value === 'establishment_data') return product.establishment_data?.name || 'N/A'
   else if (value === 'is_management') return product.is_management ? 'Sim' : 'Não'
+  else if (value === 'responsible_person_data') return product.responsible_person_data?.name || 'N/A'
   return product[value]
 }
 </script>
@@ -25,43 +47,40 @@ const formatField = (product, value) => {
   <div class="mt-6">
 
 
-
-
-  <div class="md:hidden space-y-4">
-    <div
-      v-for="product in products"
-      :key="product.id"
-      class="border border-neutral-300 rounded-lg p-4 shadow-sm"
-    >
-  <div
-    v-for="header in headers"
-    :key="header.value"
-    class="flex justify-between py-1"
-  >
-    <span class="font-medium text-neutral-600 text-sm">{{ header.name }}</span>
-    <span class="text-sm text-neutral-800">
+    <div class="md:hidden space-y-4">
+      <div
+        v-for="product in products"
+        :key="product.id"
+        class="border border-neutral-300 rounded-lg p-4 shadow-sm"
+      >
+        <div
+          v-for="header in headers"
+          :key="header.value"
+          class="flex justify-between py-1"
+        >
+          <span class="font-medium text-neutral-600 text-sm">{{ header.name }}</span>
+          <span class="text-sm text-neutral-800">
       {{ formatField(product, header.value) }}
     </span>
-  </div>
+        </div>
 
-  <div class="flex flex-row items-center gap-3 pt-3">
-    <div class="cursor-pointer hover:bg-gray-200 rounded-lg p-1 transition-all">
-      <PencilLine @click="modalStore.openCreateModal('edit', product)" :size="20" />
+        <div class="flex flex-row items-center gap-3 pt-3">
+          <div class="cursor-pointer hover:bg-gray-200 rounded-lg p-1 transition-all">
+            <PencilLine @click="modalStore.openCreateModal('edit', product)" :size="20"/>
+          </div>
+          <div v-if="props.canDelete === true" class="cursor-pointer hover:bg-gray-200 rounded-lg p-1 transition-all">
+            <Trash :size="20" @click="modalStore.openConfirmDeleteModal(product.id)"/>
+          </div>
+        </div>
+      </div>
+
     </div>
-    <div class="cursor-pointer hover:bg-gray-200 rounded-lg p-1 transition-all">
-      <Trash :size="20" @click="modalStore.openConfirmDeleteModal(product.id)" />
-    </div>
-  </div>
-</div>
-
-  </div>
 
 
-
-  <div class="w-full overflow-x-auto hidden md:block">
-    <table
-      class="border border-neutral-300 rounded-xl border-separate border-spacing-0 table-fixed w-full min-w-[600px]">
-      <thead>
+    <div class="w-full overflow-x-auto hidden md:block">
+      <table
+        class="border border-neutral-300 rounded-xl border-separate border-spacing-0 table-fixed w-full min-w-[600px]">
+        <thead>
         <tr>
           <th
             v-for="header in headers"
@@ -74,37 +93,37 @@ const formatField = (product, value) => {
             Ações
           </th>
         </tr>
-      </thead>
+        </thead>
 
 
-  <tbody>
-    <tr v-for="product in products" :key="product.id" class="border-t border-neutral-300">
+        <tbody>
+        <tr v-for="product in products" :key="product.id" class="border-t border-neutral-300">
 
-      <td
-        v-for="header in headers"
-        :key="header.value"
-        class="p-2 border-t border-neutral-300 break-words whitespace-normal"
-      >
-        {{ formatField(product, header.value) }}
-      </td>
+          <td
+            v-for="header in headers"
+            :key="header.value"
+            class="p-2 border-t border-neutral-300 break-words whitespace-normal"
+          >
+            {{ formatField(product, header.value) }}
+          </td>
 
-      <td class="p-2 border-t border-neutral-300">
-        <div class="flex flex-row items-center justify-start gap-2">
-          <div class="cursor-pointer hover:bg-gray-300 rounded-lg p-1 transition-all">
-            <PencilLine @click="modalStore.openCreateModal('edit', product)" :size="20" />
-          </div>
-          <div class="cursor-pointer hover:bg-gray-300 rounded-lg p-1 transition-all">
-            <Trash :size="20" @click="modalStore.openConfirmDeleteModal(product.id)" />
-          </div>
-        </div>
-      </td>
+          <td class="p-2 border-t border-neutral-300">
+            <div class="flex flex-row items-center justify-start gap-2">
+              <div class="cursor-pointer hover:bg-gray-300 rounded-lg p-1 transition-all">
+                <PencilLine @click="modalStore.openCreateModal('edit', product)" :size="20"/>
+              </div>
+              <div v-if="props.canDelete === true" class="cursor-pointer hover:bg-gray-300 rounded-lg p-1 transition-all">
+                <Trash :size="20" @click="modalStore.openConfirmDeleteModal(product.id)"/>
+              </div>
+            </div>
+          </td>
 
-    </tr>
-  </tbody>
-</table>
+        </tr>
+        </tbody>
+      </table>
 
 
-  </div>
+    </div>
   </div>
 
 </template>
---
+++ src/views/OrdersView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ProductsTable from "@/components/management-menu/ProductsTable.vue";
+import {onMounted} from "vue";
+import {useOrderStore} from "@/stores/order.js";
+
+const orderStore = useOrderStore();
+
+const headers = [
+  {name: 'Status', value: 'status'},
+  {name: 'Cliente', value: 'customer'},
+  {name: 'Data do Pedido', value: 'order_date'},
+  {name: 'Responsável', value: 'responsible_person_data'},
+  {name: 'Total', value: 'total_amount'},
+  {name: 'Pago', value: 'is_paid'}
+]
+
+onMounted(() => {
+  orderStore.fetchOrders();
+});
+</script>
+
+<template>
+  <main class="w-full h-full px-12 py-8">
+    <h1 class="text-4xl font-bold">Pedidos</h1>
+    <ProductsTable :headers="headers" :products="orderStore.orders" :can-delete="false" />
+  </main>
+</template>
