fc22e3c6c8293d2ca4c97ad6cc803fa8cdcf811c
guiottijoao <joaovitorguiotti8@gmail.com>
2025-12-01 16:01:07 -0300

    FEAT: Display final cups and combos details of each order item #57

--- src/components/Orders/CreatedOrderItem.vue
+++ src/components/Orders/CreatedOrderItem.vue
@@ -35,7 +35,15 @@ const getItemIconColor = (type) => {
 };
 
 const customCup = computed(() => {
-  return orderItemStore.getCustomCup(props.orderItem.custom_cup)
+  return orderItemStore.getCupById(props.orderItem.custom_cup, 'CUSTOM_CUP');
+})
+
+const finalCup = computed(() => {
+  return orderItemStore.getCupById(props.orderItem.final_cup, 'FINAL_CUP');
+})
+
+const combo = computed(() => {
+  return orderItemStore.getCupById(props.orderItem.combo, 'COMBO');
 })
 
 const recipientName = computed(() => {
@@ -67,7 +75,7 @@ const ingredientsNames = computed(() => {
         </div>
 
         <!-- Detalhes do Item (CUSTOM CUP) -->
-        <div class="ml-6 space-y-1" v-if="orderItem.type === 'CUSTOM_CUP'">
+        <div v-if="orderItem.type === 'CUSTOM_CUP'" class="ml-6 space-y-1">
           <div
             class="text-sm"
           >
@@ -80,6 +88,26 @@ const ingredientsNames = computed(() => {
           >
             <span class="text-gray-500">Ingredientes:</span>
             <span class="text-gray-700 ml-1">{{ ingredientsNames }}</span>
+          </div>
+        </div>
+
+        <!-- Detalhes do Item (FINAL CUP) -->
+        <div v-else-if="orderItem.type === 'FINAL_CUP'" class="ml-6 space-y-1">
+          <div
+            class="text-sm"
+          >
+            <span class="text-gray-500">Nome:</span>
+            <span class="text-gray-700 ml-1">{{ finalCup.name }}</span>
+          </div>
+        </div>
+
+        <!-- Detalhes do Item (COMBO) -->
+        <div v-else-if="orderItem.type === 'COMBO'" class="ml-6 space-y-1">
+          <div
+            class="text-sm"
+          >
+            <span class="text-gray-500">Nome:</span>
+            <span class="text-gray-700 ml-1">{{ combo.name }}</span>
           </div>
         </div>
       </div>
