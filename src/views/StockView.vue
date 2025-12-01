5566fac82906864cfa39649a9acd89eb5da7ac8c
guiottijoao <joaovitorguiotti8@gmail.com>
2025-12-01 20:35:16 -0300

    FEAT: Remove deleted field unit_of_measure in stock entity #57

--- src/views/StockView.vue
+++ src/views/StockView.vue
@@ -22,10 +22,9 @@ onMounted(() => {
 
 const headers = [
   { name: 'Ingrediente', value: 'ingredient_data' },
-  { name: 'Quantidade', value: 'quantity' },
+  { name: 'Quantidade (g)', value: 'quantity' },
   { name: 'Lote', value: 'batch' },
   { name: 'Fornecedor', value: 'supplier' },
-  { name: 'Un. de medida (lote)', value: 'unit_of_measure' },
   { name: 'Preço (lote)', value: 'batch_price' },
   { name: 'Validade', value: 'expiration_date' }
 ]
