ebe9af9ff9f0ea1090bb727673faaf41e33c58af
guiottijoao <joaovitorguiotti8@gmail.com>
2025-12-01 14:45:11 -0300

    CHORE: Update pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vr50crt9p9c"
+    "revision": "0.6gctddrhrj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
