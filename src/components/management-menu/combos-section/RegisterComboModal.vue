ed209b7dfb918d5810600f87b661bce5870ddaf9
guiottijoao <joaovitorguiotti8@gmail.com>
2026-01-28 23:37:43 -0300

    FEAT: Add empty state message for RegisterComboModal when no FinalCups are registered #57

--- src/components/management-menu/combos-section/RegisterComboModal.vue
+++ src/components/management-menu/combos-section/RegisterComboModal.vue
@@ -12,7 +12,7 @@ defineProps({
   title: { type: String, required: true },
   btnName: { type: String, required: true },
   mode: { type: String, required: true },
-  model: { type: Object, required: true }
+  model: { type: Object, required: true },
 })
 
 defineEmits(['createCombo', 'editCombo'])
@@ -24,19 +24,22 @@ onMounted(() => {
 
 <template>
   <main
-    class="w-100 h-fit bg-white rounded-3xl shadow-xl p-6 pt-8 flex flex-col items-center gap-6 mx-4 relative md:mx-0">
+    class="w-100 h-fit bg-white rounded-3xl shadow-xl p-6 pt-8 flex flex-col items-center gap-6 mx-4 relative md:mx-0"
+  >
     <h1 class="text-3xl font-bold mb-2 text-center">{{ title }}</h1>
     <div
       class="h-8 w-8 rounded-full hover:bg-neutral-200 flex items-center justify-center absolute top-4 right-4 cursor-pointer"
-      @click="modalStore.closeCreateModal">
+      @click="modalStore.closeCreateModal"
+    >
       <X />
     </div>
 
-    <form @submit.prevent="$emit(mode === 'create' ? 'createCombo' : 'editCombo')"
-          class="w-full flex flex-col gap-6">
+    <form
+      @submit.prevent="$emit(mode === 'create' ? 'createCombo' : 'editCombo')"
+      class="w-full flex flex-col gap-6"
+    >
       <section class="w-full grid grid-cols-1 gap-4">
-        <div
-          class="flex flex-col gap-1 align-center w-full col-span-2">
+        <div class="flex flex-col gap-1 align-center w-full col-span-2">
           <label for="name">Nome</label>
           <input
             id="name"
@@ -52,14 +55,18 @@ onMounted(() => {
           <MoneyInput v-model="model.price" />
         </div>
 
-        <section class="gap-1 flex flex-col col-span-2">
+        <section v-if="finalCupStore.finalCups.length > 0" class="gap-1 flex flex-col col-span-2">
           <label for="finalCup">Produtos</label>
           <div class="max-h-40 overflow-y-scroll border border-neutral-300 rounded-xl">
             <div v-for="finalCup in finalCupStore.finalCups" :key="finalCup.id">
               <div class="flex justify-between align-center p-4">
                 <label :for="finalCup.id">{{ finalCup.name }}</label>
-                <input class="w-4 cursor-pointer" v-model="model.final_cup" :value="finalCup.id"
-                       type="checkbox" />
+                <input
+                  class="w-4 cursor-pointer"
+                  v-model="model.final_cup"
+                  :value="finalCup.id"
+                  type="checkbox"
+                />
               </div>
               <div class="px-4">
                 <hr class="border-neutral-300" />
@@ -67,9 +74,16 @@ onMounted(() => {
             </div>
           </div>
         </section>
+
+        <section v-else class="gap-1 flex flex-col col-span-2">
+          <label for="finalCup">Produtos</label>
+          <p class="text-gray-600">Nenhum produto cadastrado.</p>
+        </section>
       </section>
-      <button type="submit"
-              class="w-full h-15 rounded-xl bg-rose-900 font-medium text-white hover:bg-rose-950 cursor-pointer duration-200 ease-in-out">
+      <button
+        type="submit"
+        class="w-full h-15 rounded-xl bg-rose-900 font-medium text-white hover:bg-rose-950 cursor-pointer duration-200 ease-in-out"
+      >
         {{ btnName }}
       </button>
     </form>
